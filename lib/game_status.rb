4024e1b13bada902e215aaaa3564442d46eb4fe4
Daiane Bushey <daianebushey@gmail.com>
2017-11-20 21:28:37 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,38 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [0,3,6], # Left column
+  [1,4,7], # Middle column
+  [2,5,8], # Right column
+  [0,4,8], # Diagonal 1
+  [6,4,2]  # Diagonal 2
+]
+
+def won?(board)
+
+  WIN_COMBINATIONS.each do |win_combination|
+    win_combination.each do |win_index|
+
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1] # load the value of the board at win_index_1
+    position_2 = board[win_index_2] # load the value of the board at win_index_2
+    position_3 = board[win_index_3] # load the value of the board at win_index_3
+
+      if position_1 == "X" && position_2 == "X" && position_3 == "X" ||
+         position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return [win_index_1, win_index_2, win_index_3]
+      else
+        return false
+      end
+
+    end
+  end
+
+end
